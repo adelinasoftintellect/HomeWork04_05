b3d214fe58294c3e50a6f7b2429874e2182fa967
Adi <adelina_yordanova11@abv.bg>
2016-05-21 16:19:23 +0300

    HomeWork04_05.

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		// int size = a.length;
+		// int num = 7;
+		// int low = 0;
+		// int high = size - 1;
+		// while (high >= low) {
+		// int middle = (low + high) / 2;
+		// if (middle == num) {
+		// System.out.print("Number found");
+		// }
+		// if (middle < num) {
+		// low = middle + 1;
+		// }else if (middle > num) {
+		// high = middle - 1;
+		// } else {
+		// System.out.print("Number NOT found");
+		// }
+		//
+		// }
+	}
+}
