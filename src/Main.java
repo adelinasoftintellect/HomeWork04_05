114a2c990546c25dd589ea13d7d7b0aa71b9ac4a
Adi <adelina_yordanova11@abv.bg>
2016-05-23 10:37:19 +0300

    HomeWork_04_05.

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws java.lang.Exception {
 		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + " ");
+		System.out.println(Arrays.toString(a));
+
+		int num = 7;
+		int low = 0;
+		int high = a.length - 1;
+		while (high >= low) {
+			int middle = (low + high) / 2;
+			if (a[middle] == num) {
+				System.out.print(middle);
+				break;
+			}
+			if (a[middle] < num) {
+				low = middle + 1;
+			} else if (a[middle] > num) {
+				high = middle - 1;
+			} else if (low >= high) {
+				System.out.print("Number NOT found");
+				break;
+			}
 		}
-		// int size = a.length;
-		// int num = 7;
-		// int low = 0;
-		// int high = size - 1;
-		// while (high >= low) {
-		// int middle = (low + high) / 2;
-		// if (middle == num) {
-		// System.out.print("Number found");
-		// }
-		// if (middle < num) {
-		// low = middle + 1;
-		// }else if (middle > num) {
-		// high = middle - 1;
-		// } else {
-		// System.out.print("Number NOT found");
-		// }
-		//
-		// }
 	}
 }
